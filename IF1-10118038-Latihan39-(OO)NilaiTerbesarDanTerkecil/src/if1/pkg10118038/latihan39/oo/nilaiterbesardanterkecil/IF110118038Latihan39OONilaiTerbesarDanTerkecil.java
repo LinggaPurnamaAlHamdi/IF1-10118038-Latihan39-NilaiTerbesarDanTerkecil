ffac18162a5bce8b410bc499ab935fa9d6c37a36
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-10-17 11:23:43 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan39-(OO)NilaiTerbesarDanTerkecil/src/if1/pkg10118038/latihan39/oo/nilaiterbesardanterkecil/IF110118038Latihan39OONilaiTerbesarDanTerkecil.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan39.oo.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : program ini berisi program yang akan menampilkan Nilai Terbesar Dan Terkecil
+ */
+public class IF110118038Latihan39OONilaiTerbesarDanTerkecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nilai nilai = new Nilai();
+	nilai.inputPetugas();
+	nilai.inputNilai();
+	nilai.outputNilai();
+        nilai.outputPetugas();
+    }
+    
+}
